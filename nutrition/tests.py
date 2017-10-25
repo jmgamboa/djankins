8fe9419d9d440f689364ca8b6896e8fc0012010d
Joaquin Gamboa <gamboanj@gmail.com>
2017-10-25 10:51:40 -0400

    bug fixes

--- nutrition/tests.py
+++ nutrition/tests.py
@@ -3,6 +3,6 @@ from django.test import TestCase
 # Create your tests here.
 class TestQuestion(TestCase):
 
-	def test_question(self)
+	def test_question(self):
 		print 'made it to the test question'
 		assert 1 == 1
